5daf64415d5f7ba186de697b4b860e73d2e9b77a
Vivesh kumar <vivesh103@gmail.com>
2025-01-25 21:15:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function makeid(l) {
   // write your code here
+  let result = " ";
+   let char_list ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for(let i =0;i<l;i++){
+		result += char_list.charAt(math.floor(math.random()*char_list.length));
+	}
+	return result;
 }
+
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
